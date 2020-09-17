13583b646f4812acef6c9c00ac40347d957ab78a
dmmpwa <71463080+dmmpwa@users.noreply.github.com>
2020-09-17 14:16:43 -0500

    Create mi-footer.js

---
+++ cmp/mi-footer.js
@@ -0,0 +1,7 @@
+class MiFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html*/
+      `Copyright &copy; Gilberto Pachecco Gallegos`;
+  }
+}
+customElements.define("mi-footer", MiFooter);
