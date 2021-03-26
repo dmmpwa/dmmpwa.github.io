d76a3ccf5b57db06fbc5f1d2e9c8988d943bb77b
gilpgmisitio <gilpgmisitio@gmail.com>
2021-03-26 16:29:17 -0600

    sw 2.01

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  * cambios en tu sitio, para
  * depués actualizar este archivo.
  */
-const CACHE = "dmppwa-2.00";
+const CACHE = "dmppwa-2.01";
 
 /** Archivos requeridos para que
  * la aplicación funcione fuera de
