16c8406c53532837aa58a8a72e79f00edb16f148
gilpgmisitio <gilpgmisitio@gmail.com>
2021-03-26 15:52:19 -0600

    Quita lista.txt

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  * cambios en tu sitio, para
  * depués actualizar este archivo.
  */
-const CACHE = "dmppwa-2.000";
+const CACHE = "dmppwa-2.00";
 
 /** Archivos requeridos para que
  * la aplicación funcione fuera de
